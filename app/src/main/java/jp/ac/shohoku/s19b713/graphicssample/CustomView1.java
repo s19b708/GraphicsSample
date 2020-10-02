37beead0d0861b0c82bc86e48e3bc02fbf7b60cd
19b713 <s19b713@shohoku.ac.jp>
2020-10-02 11:14:15 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/shohoku/s19b713/graphicssample/CustomView1.java
@@ -0,0 +1,26 @@
+package jp.ac.shohoku.s19b713.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView1 extends View{
+
+    public CustomView1(Context context) {
+        super(context);
+    }
+
+    public CustomView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
---
+++ app/src/main/java/jp/ac/shohoku/s19b713/graphicssample/CustomView2.java
@@ -0,0 +1,26 @@
+package jp.ac.shohoku.s19b713.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView2 extends View{
+
+    public CustomView2(Context context) {
+        super(context);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
