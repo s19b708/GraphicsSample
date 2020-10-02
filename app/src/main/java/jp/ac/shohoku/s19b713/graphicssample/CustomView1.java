b2f8fabd53cd827be5dd97a4b2dd2194821647a7
s19B708 <s19b708@shohoku.ac.jp>
2020-10-02 12:24:54 +0900

    コード書きました。確認お願いします。

--- app/src/main/java/jp/ac/shohoku/s19b713/graphicssample/CustomView1.java
+++ app/src/main/java/jp/ac/shohoku/s19b713/graphicssample/CustomView1.java
@@ -1,6 +1,11 @@
 package jp.ac.shohoku.s19b713.graphicssample;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,7 +25,24 @@ public class CustomView1 extends View{
         super(context, attrs, defStyleAttr);
     }
 
-    public CustomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    protected void onDraw(Canvas  canvas){
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+
+        int w = this.getWidth();
+        int h = this.getHeight();
+        Paint p = new Paint();
+        p.setStyle(Style.STROKE);
+        p.setColor(Color.DKGRAY);
+        canvas.drawRect(new Rect(5, 5, w - 10, h - 10), p);
+
+        for(int i = 0; i<10; i++){
+            Paint p2 = new Paint();
+            p2.setStyle(Style.FILL);
+            p2.setColor(Color.rgb(25*i, 0, 0));
+            canvas.drawCircle(25*i+125, 25*i+125, 100, p2);
+        }
+
     }
+
 }
